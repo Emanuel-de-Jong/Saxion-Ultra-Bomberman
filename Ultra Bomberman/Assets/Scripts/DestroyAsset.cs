fcf508852bc508672f795222a7365d3d45b0b33c
rgrado <git@rgrado.nl>
2020-11-24 11:21:36 +0100

    File structure

---
+++ Ultra Bomberman/Assets/Scripts/DestroyAsset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAsset : MonoBehaviour
+{
+    public int destroyDelay;
+    private float timeTillDestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeTillDestroy = Time.time + destroyDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeTillDestroy <= Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ultra Bomberman/Assets/Scripts/Dynamite/DynamiteCycle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamiteCycle : MonoBehaviour
+{
+
+    public GameObject Dynamite;
+    public GameObject Explosion;
+    public GameObject Flame;
+    public int explosionDelay;
+
+    public int rotationZ;
+
+    private GameObject temp_flame;
+    private GameObject temp_explosion;
+
+    private float timeTillExplosion;
+
+    void Start()
+    {
+        timeTillExplosion = Time.time + explosionDelay;
+        spawnFlame();
+    }
+
+    void spawnFlame()
+    {
+        temp_flame = Instantiate(Flame, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0f, rotationZ, 0)));
+        temp_flame.transform.position += temp_flame.transform.forward * 0.7f;
+    }
+
+    void Update()
+    {
+        if (timeTillExplosion <= Time.time)
+        {
+            Instantiate(Explosion, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+
+            Destroy(temp_flame);
+            Destroy(gameObject);
+        }
+    }
+}
