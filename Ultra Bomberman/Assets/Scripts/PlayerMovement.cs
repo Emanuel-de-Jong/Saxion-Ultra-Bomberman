bc2c7ff25aded5f5ad74b840c26d567a19e4d9ec
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2020-12-15 14:09:51 +0100

    Player movement script code improvements and keeping the previous direction.

--- Ultra Bomberman/Assets/Scripts/PlayerMovement.cs
+++ Ultra Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float rayOffsetZ = 1f;
 
+    enum Direction
+    {
+        Forward,
+        Back,
+        Left,
+        Right,
+        None
+    }
+
     Vector3 xOffset;
     Vector3 yOffset;
     Vector3 zOffset;
@@ -24,15 +33,15 @@ public class PlayerMovement : MonoBehaviour
     Vector3 xAxisOriginA;
     Vector3 xAxisOriginB;
 
-    Vector3 targetPosition;
-    Vector3 startPosition;
+    Vector3 offset;
 
     Animator animator;
     private Rigidbody rigidbody;
 
     string rotation = "FORWARD";
-
-    bool moving;
+    Direction lastDir = Direction.Forward;
+    Dictionary<Direction, bool> input = new Dictionary<Direction, bool>();
+    bool moved;
 
     void Start()
     {
@@ -42,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-
         yOffset = transform.position + Vector3.up * rayOffsetY;
         zOffset = Vector3.forward * rayOffsetZ;
         xOffset = Vector3.right * rayOffsetX;
@@ -53,82 +61,82 @@ public class PlayerMovement : MonoBehaviour
         xAxisOriginA = yOffset + zOffset;
         xAxisOriginB = yOffset - zOffset;
 
-        bool w = Input.GetKey(KeyCode.W);
-        bool a = Input.GetKey(KeyCode.A);
-        bool s = Input.GetKey(KeyCode.S);
-        bool d = Input.GetKey(KeyCode.D);
-
         rigidbody.angularVelocity = Vector3.zero;
 
-        if (w)
+        input[Direction.Forward] = Input.GetKey(KeyCode.W);
+        input[Direction.Back] = Input.GetKey(KeyCode.S);
+        input[Direction.Left] = Input.GetKey(KeyCode.A);
+        input[Direction.Right] = Input.GetKey(KeyCode.D);
+
+        moved = false;
+        if (lastDir != Direction.None && input[lastDir])
         {
-            targetPosition = transform.position + Vector3.forward;
-            startPosition = transform.position;
-
-            if (!rotation.Equals("FORWARD"))
-            {
-                transform.localRotation = Quaternion.Euler(0, 0, 0);
-                rotation = "FORWARD";
-            }
-
-            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
-
-            moving = true;
-            animator.SetBool("isWalking", true);
-        } 
-        else if (s)
-        {
-            targetPosition = transform.position + Vector3.back;
-            startPosition = transform.position;
-
-            if (!rotation.Equals("BACK"))
-            {
-                transform.localRotation = Quaternion.Euler(0, 180, 0);
-                rotation = "BACK";
-            }
-
-            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
-
-            moving = true;
-            animator.SetBool("isWalking", true);
+            move(lastDir);
         }
-        else if (a)
-        {
-            targetPosition = transform.position + Vector3.left;
-            startPosition = transform.position;
-
-            if (!rotation.Equals("LEFT"))
-            {
-                transform.localRotation = Quaternion.Euler(0, 270, 0);
-                rotation = "LEFT";
-            }
-
-            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
-
-            moving = true;
-            animator.SetBool("isWalking", true);
-        }
-        else if (d)
-        {
-            targetPosition = transform.position + Vector3.right;
-            startPosition = transform.position;
-
-            if (!rotation.Equals("RIGHT"))
-            {
-                transform.localRotation = Quaternion.Euler(0, 90, 0);
-                rotation = "RIGHT";
-            }
-
-            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
-
-            moving = true;
-            animator.SetBool("isWalking", true);
-        } 
         else
         {
-            moving = false;
-            animator.SetBool("isWalking", false);
+            foreach (KeyValuePair<Direction, bool> entry in input)
+            {
+                if (entry.Value)
+                {
+                    move(entry.Key);
+                    lastDir = entry.Key;
+                    break;
+                }
+            }
         }
 
+        if (moved)
+        {
+            transform.position += offset * movementSpeed * Time.deltaTime;
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            lastDir = Direction.None;
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    void move(Direction dir)
+    {
+        moved = true;
+
+        if (dir == Direction.Forward)
+        {
+            offset = Vector3.forward;
+            if (!rotation.Equals("FORWARD"))
+            {
+                rotation = "FORWARD";
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        else if (dir == Direction.Back)
+        {
+            offset = Vector3.back;
+            if (!rotation.Equals("BACK"))
+            {
+                rotation = "BACK";
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+        else if (dir == Direction.Left)
+        {
+            offset = Vector3.left;
+            if (!rotation.Equals("LEFT"))
+            {
+                rotation = "LEFT";
+                transform.localRotation = Quaternion.Euler(0, 270, 0);
+            }
+        }
+        else if (dir == Direction.Right)
+        {
+            offset = Vector3.right;
+            if (!rotation.Equals("RIGHT"))
+            {
+                rotation = "RIGHT";
+                transform.localRotation = Quaternion.Euler(0, 90, 0);
+            }
+        }
     }
 }
