f54635a52bcb19623de299dbe026d00e19bc3502
rgrado <git@rgrado.nl>
2020-12-15 11:56:27 +0100

    Movement done

--- Ultra Bomberman/Assets/Scripts/PlayerMovement.cs
+++ Ultra Bomberman/Assets/Scripts/PlayerMovement.cs
@@ -28,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 startPosition;
 
     Animator animator;
+    private Rigidbody rigidbody;
 
     string rotation = "FORWARD";
 
@@ -36,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -56,107 +58,77 @@ public class PlayerMovement : MonoBehaviour
         bool s = Input.GetKey(KeyCode.S);
         bool d = Input.GetKey(KeyCode.D);
 
-        if (moving)
-        {
-
-            if (Vector3.Distance(startPosition, transform.position) > 1f)
-            {
-                transform.position = targetPosition;
-                moving = false;
-                return;
-            }
-
-            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
-            return;
-        }
+        rigidbody.angularVelocity = Vector3.zero;
 
         if (w)
         {
-            if (CanMove(Vector3.forward))
+            targetPosition = transform.position + Vector3.forward;
+            startPosition = transform.position;
+
+            if (!rotation.Equals("FORWARD"))
             {
-                targetPosition = transform.position + Vector3.forward;
-                startPosition = transform.position;
-
-                if (!rotation.Equals("FORWARD"))
-                {
-                    transform.localRotation = Quaternion.Euler(0, 0, 0);
-                    rotation = "FORWARD";
-                }
-
-                moving = true;
-                animator.SetBool("isWalking", true);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                rotation = "FORWARD";
             }
+
+            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
+
+            moving = true;
+            animator.SetBool("isWalking", true);
         } 
         else if (s)
         {
-            if (CanMove(Vector3.back))
+            targetPosition = transform.position + Vector3.back;
+            startPosition = transform.position;
+
+            if (!rotation.Equals("BACK"))
             {
-                targetPosition = transform.position + Vector3.back;
-                startPosition = transform.position;
-
-                if (!rotation.Equals("BACK"))
-                {
-                    transform.localRotation = Quaternion.Euler(0, 180, 0);
-                    rotation = "BACK";
-                }
-
-                moving = true;
-                animator.SetBool("isWalking", true);
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                rotation = "BACK";
             }
+
+            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
+
+            moving = true;
+            animator.SetBool("isWalking", true);
         }
         else if (a)
         {
-            if (CanMove(Vector3.left))
+            targetPosition = transform.position + Vector3.left;
+            startPosition = transform.position;
+
+            if (!rotation.Equals("LEFT"))
             {
-                targetPosition = transform.position + Vector3.left;
-                startPosition = transform.position;
-
-                if (!rotation.Equals("LEFT"))
-                {
-                    transform.localRotation = Quaternion.Euler(0, 270, 0);
-                    rotation = "LEFT";
-                }
-
-                moving = true;
-                animator.SetBool("isWalking", true);
+                transform.localRotation = Quaternion.Euler(0, 270, 0);
+                rotation = "LEFT";
             }
+
+            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
+
+            moving = true;
+            animator.SetBool("isWalking", true);
         }
         else if (d)
         {
-            if (CanMove(Vector3.right))
+            targetPosition = transform.position + Vector3.right;
+            startPosition = transform.position;
+
+            if (!rotation.Equals("RIGHT"))
             {
-                targetPosition = transform.position + Vector3.right;
-                startPosition = transform.position;
-
-                if (!rotation.Equals("RIGHT"))
-                {
-                    transform.localRotation = Quaternion.Euler(0, 90, 0);
-                    rotation = "RIGHT";
-                }
-
-                moving = true;
-                animator.SetBool("isWalking", true);
+                transform.localRotation = Quaternion.Euler(0, 90, 0);
+                rotation = "RIGHT";
             }
+
+            transform.position += (targetPosition - startPosition) * movementSpeed * Time.deltaTime;
+
+            moving = true;
+            animator.SetBool("isWalking", true);
         } 
         else
         {
+            moving = false;
             animator.SetBool("isWalking", false);
         }
 
-    }
-
-    bool CanMove(Vector3 direction)
-    {
-        if (direction.z != 0)
-        {
-            if (Physics.Raycast(zAxisOriginA, direction, rayLength)) return false;
-            if (Physics.Raycast(zAxisOriginB, direction, rayLength)) return false;
-        }
-        else if (direction.x != 0)
-        {
-            if (Physics.Raycast(xAxisOriginA, direction, rayLength)) return false;
-            if (Physics.Raycast(xAxisOriginB, direction, rayLength)) return false;
-        }
-        return true;
     }
 }
