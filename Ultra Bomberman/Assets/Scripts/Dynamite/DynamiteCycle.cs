eb8e0cca3bf6991a4132c6c97caa831d30212cd9
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-01-08 15:02:21 +0100

    Bomb restructure part 2. Merged dynamite and fire.

--- Ultra Bomberman/Assets/Scripts/Dynamite/DynamiteCycle.cs
+++ Ultra Bomberman/Assets/Scripts/Dynamite/DynamiteCycle.cs
@@ -4,98 +4,117 @@ using UnityEngine;
 
 public class DynamiteCycle : MonoBehaviour
 {
-    public float range;
+    public float range = 2f;
 
-    public GameObject Dynamite;
-    public GameObject Explosion;
-    public GameObject Trail;
-    public GameObject Flame;
-    public int explosionDelay;
+    [SerializeField] int trailLength = 9;
+    [SerializeField] float gridSize = 2f;
+    [SerializeField] GameObject explosion;
+    [SerializeField] GameObject trail;
+    [SerializeField] float explosionDelay = 3f;
+    [SerializeField] int[] raycastPoints = new int[] { 0, 90, 180, 270 };
 
-    public int rotationZ = 0;
-
-    private GameObject temp_flame;
-
-    private float timeTillExplosion;
-    public float bonusRange = 0;
     public static float defaultTrailDistance = 0.6f;
 
-    private float forwardTrailDistance = defaultTrailDistance;
-    private float rightTrailDistance = defaultTrailDistance;
-    private float bottomTrailDistance = defaultTrailDistance;
-    private float leftTrailDistance = defaultTrailDistance;
+    float forwardTrailDistance = defaultTrailDistance;
+    float rightTrailDistance = defaultTrailDistance;
+    float bottomTrailDistance = defaultTrailDistance;
+    float leftTrailDistance = defaultTrailDistance;
+
+    GameObject[] trails;
+    float[] trailDistances;
 
     void Start()
     {
-        timeTillExplosion = Time.time + explosionDelay;
-        spawnFlame();
-    }
+        range *= gridSize;
+        trails = new GameObject[raycastPoints.Length];
+        trailDistances = new float[raycastPoints.Length];
+        Invoke(nameof(Explode), explosionDelay);
 
-    void spawnFlame()
-    {
-        temp_flame = Instantiate(Flame, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0f, rotationZ, 0)));
-        temp_flame.transform.position += temp_flame.transform.forward * 0.7f;
-    }
-
-    void Update()
-    {
-        if (timeTillExplosion <= Time.time)
+        // Debug Raycast
+        Vector3 pos = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        for (int i = 0; i < raycastPoints.Length; i++)
         {
-            Instantiate(Explosion, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0, 0, 0)));
-
-            spawnTrail();
-
-            Destroy(temp_flame);
-            Destroy(gameObject);
-        }
-
-        Debug.DrawRay(transform.position, Vector3.forward * 10, Color.red);
-        RaycastHit forwardHitInfo;
-
-        if (Physics.Raycast(transform.position, Vector3.forward, out forwardHitInfo)) 
-        {
-            if ((forwardHitInfo.distance / 1.5) < defaultTrailDistance + bonusRange)
+            Vector3 dir;
+            switch (i)
             {
-                forwardTrailDistance = forwardHitInfo.distance / 2;
-                Debug.Log(forwardHitInfo.distance);
+                case 0:
+                    dir = Vector3.forward;
+                    break;
+                case 1:
+                    dir = Vector3.right;
+                    break;
+                case 2:
+                    dir = Vector3.back;
+                    break;
+                case 3:
+                    dir = Vector3.left;
+                    break;
+                default:
+                    dir = Vector3.forward;
+                    break;
+            }
+
+            Debug.DrawRay(pos, dir * range, Color.red, explosionDelay);
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosion, new Vector3(transform.position.x, explosion.transform.position.y, transform.position.z), explosion.transform.rotation);
+
+        CalculateTrailDinstances();
+
+        SpawnTrail();
+
+        Destroy(gameObject);
+    }
+
+    void CalculateTrailDinstances()
+    {
+        Vector3 pos = new Vector3(transform.position.x, 0.5f, transform.position.z);
+
+        for (int i = 0; i < raycastPoints.Length; i++)
+        {
+            Vector3 dir;
+            switch (i)
+            {
+                case 0:
+                    dir = Vector3.forward;
+                    break;
+                case 1:
+                    dir = Vector3.right;
+                    break;
+                case 2:
+                    dir = Vector3.back;
+                    break;
+                case 3:
+                    dir = Vector3.left;
+                    break;
+                default:
+                    dir = Vector3.forward;
+                    break;
+            }
+
+            if (Physics.Raycast(pos, dir, out RaycastHit hit, range))
+            {
+                trailDistances[i] = hit.distance;
             }
         }
-
-        Debug.DrawRay(transform.position, Vector3.right * 10, Color.red);
-        RaycastHit rightHitInfo;
-
-        if (Physics.Raycast(transform.position, Vector3.right, out rightHitInfo))
-        {
-            Debug.Log(rightHitInfo.distance);
-        }
-
-        Debug.DrawRay(transform.position, Vector3.back * 10, Color.red);
-        RaycastHit backHitInfo;
-
-        if (Physics.Raycast(transform.position, Vector3.back, out backHitInfo))
-        {
-            Debug.Log(backHitInfo.distance);
-        }
-
-        Debug.DrawRay(transform.position, Vector3.left * 10, Color.red);
-        RaycastHit leftHitInfo;
-
-        if (Physics.Raycast(transform.position, Vector3.back, out leftHitInfo))
-        {
-            Debug.Log(leftHitInfo.distance);
-        }
     }
 
-    void spawnTrail()
+    void SpawnTrail()
     {
-        GameObject topTrail = Instantiate(Trail, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0, 0, 0)));
-        GameObject rightTrail = Instantiate(Trail, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0, 90, 0)));
-        GameObject bottomTrail = Instantiate(Trail, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0, 180, 0)));
-        GameObject leftTrail = Instantiate(Trail, new Vector3(transform.position.x, 0.65f, transform.position.z), Quaternion.Euler(new Vector3(0, 270, 0)));
+        Vector3 trailPos = new Vector3(transform.position.x, trail.transform.position.y, transform.position.z);
+        float xRotation = trail.transform.rotation.x, zRotation = trail.transform.rotation.z;
+        for (int i=0; i<raycastPoints.Length; i++)
+        {
+            trails[i] = Instantiate(trail, trailPos, Quaternion.Euler(xRotation, raycastPoints[i], zRotation));
+        }
 
-        topTrail.transform.localScale = new Vector3(1, 1, forwardTrailDistance + bonusRange);
-        rightTrail.transform.localScale = new Vector3(1, 1, rightTrailDistance + bonusRange);
-        bottomTrail.transform.localScale = new Vector3(1, 1, bottomTrailDistance + bonusRange);
-        leftTrail.transform.localScale = new Vector3(1, 1, leftTrailDistance + bonusRange);
+        float xScale = trail.transform.localScale.x, yScale = trail.transform.localScale.y;
+        for (int i = 0; i < raycastPoints.Length; i++)
+        {
+            trails[i].transform.localScale = new Vector3(xScale, yScale, trailDistances[i] / trailLength);
+        }
     }
 }
