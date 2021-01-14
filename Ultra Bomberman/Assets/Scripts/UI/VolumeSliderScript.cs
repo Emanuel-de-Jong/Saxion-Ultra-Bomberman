8ff2fff68e5ab504be8291b9a9fd535480326485
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-01-14 11:43:16 +0100

    UI file restructure

---
+++ Ultra Bomberman/Assets/Scripts/UI/VolumeSliderScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSliderScript : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    public void UpdateVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
