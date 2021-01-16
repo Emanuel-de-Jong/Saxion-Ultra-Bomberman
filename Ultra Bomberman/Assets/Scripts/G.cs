045893514280a1c4670b02e27f026d8dc9c5a046
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-01-16 19:12:38 +0100

    Volume sliders menuscene.

--- Ultra Bomberman/Assets/Scripts/G.cs
+++ Ultra Bomberman/Assets/Scripts/G.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public static class G
 {
     public static int playerCount = 2;
+    public static float musicVolume = 0.75f;
+    public static float effectVolume = 0.75f;
 }
--- Ultra Bomberman/Assets/Scripts/UI/SettingsMenuScript.cs
+++ Ultra Bomberman/Assets/Scripts/UI/SettingsMenuScript.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class SettingsMenuScript : MonoBehaviour
 {
-    
+    [SerializeField] AudioSource musicSource;
+
+    public void UpdateMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        G.musicVolume = volume;
+    }
+
+    public void UpdateEffectVolume(float volume)
+    {
+        G.effectVolume = volume;
+    }
 }
