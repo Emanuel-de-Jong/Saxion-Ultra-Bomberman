912917f61af0fbd912d5c54afbafcf2a2f18396b
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-01-17 15:30:14 +0100

    Simplified PlayerSpawner. Player image preperations.

--- Ultra Bomberman/Assets/Scripts/PlayerSpawner.cs
+++ Ultra Bomberman/Assets/Scripts/PlayerSpawner.cs
@@ -5,25 +5,12 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] players;
-    [SerializeField] string[] playerModels = new string[]
-       {
-            "MechanicalGolem",
-            "BarbarianGiant",
-            "BigOrk",
-            "RedDemon"
-       };
 
     public void SpawnPlayers()
     {
         for (int i=0; i<G.playerCount; i++)
         {
-            string model = playerModels[i];
-            GameObject tempPlayer = players[i];
-            PlayerController tempScript = tempPlayer.GetComponent<PlayerController>();
-
-            tempScript.playerNumber = i + 1;
-            tempScript.model = model;
-            tempPlayer.SetActive(true);
+            players[i].SetActive(true);
         }
     }
 }
