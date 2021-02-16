0afdf21c9dcfacbb77efa016f00f2d258e4fca2f
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-02-16 21:20:28 +0100

    CharacterCountMenu split between agent and player part 1.

--- Ultra Bomberman/Assets/Scripts/G.cs
+++ Ultra Bomberman/Assets/Scripts/G.cs
@@ -7,7 +7,10 @@ public static class G
     public static bool train = false;
     public static bool record = false;
 
+    public static int playerCount = 1;
+    public static int agentCount = 1;
     public static int characterCount = 2;
+
     public static int characterWon = 1;
 
     public static int roundDuration = 90;
--- Ultra Bomberman/Assets/Scripts/UI/CharacterCountMenu.cs
+++ Ultra Bomberman/Assets/Scripts/UI/CharacterCountMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,44 +7,87 @@ using UnityEngine.SceneManagement;
 
 public class CharacterCountMenu : MonoBehaviour
 {
-    [SerializeField] GameObject characterCount;
-    [SerializeField] int characterCountNumber = 2;
-    [SerializeField] int characterCountMin= 2;
-    [SerializeField] int characterCountMax = 4;
-
-    private TextMeshProUGUI characterCountTMP;
+    [SerializeField]
+    private TextMeshProUGUI playerCount;
+    [SerializeField]
+    private TextMeshProUGUI agentCount;
+    
+    private int playerCountNumber;
+    private int agentCountNumber;
+    private int characterCountNumber;
 
     void Start()
     {
-        characterCountTMP = characterCount.GetComponent<TextMeshProUGUI>();
+        playerCountNumber = Int32.Parse(playerCount.text);
+        agentCountNumber = Int32.Parse(agentCount.text);
         UpdateCharacterCount();
     }
 
-    public void DecreaseCharacterCount()
+    public void IncreasePlayerCount()
     {
-        if ((characterCountNumber - 1) >= characterCountMin)
+        if ((characterCountNumber + 1) <= 4)
         {
-            characterCountNumber--;
-            UpdateCharacterCount();
+            UpdatePlayerCount(1);
+        }
+        else if (agentCountNumber >= 1 && playerCountNumber < 4)
+        {
+            UpdatePlayerCount(1);
+            UpdateAgentCount(-1);
         }
     }
 
-    public void IncreaseCharacterCount()
+    public void DecreasePlayerCount()
     {
-        if ((characterCountNumber + 1) <= characterCountMax)
+        if ((characterCountNumber - 1) >= 2)
         {
-            characterCountNumber++;
-            UpdateCharacterCount();
+            UpdatePlayerCount(-1);
         }
+    }
+
+    public void IncreaseAgentCount()
+    {
+        if ((characterCountNumber + 1) <= 4)
+        {
+            UpdateAgentCount(1);
+        }
+        else if (playerCountNumber >= 1 && agentCountNumber < 4)
+        {
+            UpdateAgentCount(1);
+            UpdatePlayerCount(-1);
+        }
+    }
+
+    public void DecreaseAgentCount()
+    {
+        if ((characterCountNumber - 1) >= 2)
+        {
+            UpdateAgentCount(-1);
+        }
+    }
+
+    private void UpdatePlayerCount(int change)
+    {
+        playerCountNumber += change;
+        playerCount.text = playerCountNumber.ToString();
+        UpdateCharacterCount();
+    }
+
+    private void UpdateAgentCount(int change)
+    {
+        agentCountNumber += change;
+        agentCount.text = agentCountNumber.ToString();
+        UpdateCharacterCount();
     }
 
     private void UpdateCharacterCount()
     {
-        characterCountTMP.text = characterCountNumber.ToString();
+        characterCountNumber = playerCountNumber + agentCountNumber;
     }
 
     public void StartGame()
     {
+        G.playerCount = playerCountNumber;
+        G.agentCount = agentCountNumber;
         G.characterCount = characterCountNumber;
         SceneManager.LoadScene("Level1Scene", LoadSceneMode.Single);
     }
